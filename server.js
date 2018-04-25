1c74c4049b545ee39e969ed20e444483aa3e96ea
Jimmy <garry@lonfood.cn>
2018-04-25 10:58:20 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app);
+    app.use('/', express.static(__dirname));
+    server.listen(7575);
